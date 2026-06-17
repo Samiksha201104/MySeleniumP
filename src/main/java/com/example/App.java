2d9cba622748e9406ae11663c7823e756afc572f
Samiksha <samiksha.p79850@gmail.com>
2026-06-17 22:48:42 +0530

    Setup WebDriverManager for ChromeDriver

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 /**
  * Hello world!
  *
@@ -11,6 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
